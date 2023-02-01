f8b4abeaf8fce513ff12814c20078a7f856d2719
joshuaati <joshuaati97@gmail.com>
2023-02-01 18:09:36 +0100

    update ingest data file

--- week_2_workflow_orchestration/ingest_data.py
+++ week_2_workflow_orchestration/ingest_data.py
@@ -1,14 +1,15 @@
 import os
 import argparse
 from time import time
+from datetime import timedelta
 import pandas as pd
 from sqlalchemy import create_engine
 from prefect import flow, task
+from prefect.tasks import task_input_hash
 
 
-@task(log_prints=True, retries=3)
-def ingest_data(user, password, host, port, db, table_name, url):
-    
+@task(log_prints=True, retries=3,) #cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
+def extract_data(url):
     # the backup files are gzipped, and it's important to keep the correct extension
     # for pandas to be able to open the file
     if url.endswith('.csv.gz'):
@@ -17,18 +18,30 @@ def ingest_data(user, password, host, port, db, table_name, url):
         csv_name = 'output.csv'
 
     os.system(f"wget {url} -O {csv_name}")
-    postgres_url = f'postgresql://{user}:{password}@{host}:{port}/{db}'
-    engine = create_engine(postgres_url)
-
+    
     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
 
     df = next(df_iter)
 
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    return df
 
+
+@task(log_prints=True)
+def transform_data(df):
+    print(f"pre: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
+    df = df[df['passenger_count'] != 0]
+    print(f"post: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
+    return df
+
+
+@task(log_prints=True, retries=3)
+def ingest_data(user, password, host, port, db, table_name, df):
+    
+    postgres_url = f'postgresql://{user}:{password}@{host}:{port}/{db}'
+    engine = create_engine(postgres_url)
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
-
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
 
@@ -43,7 +56,9 @@ def main_flow():
     table_name = "yellow_taxi_trips"
     csv_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
 
-    ingest_data(user, password, host, port, db, table_name, csv_url)
+    raw_data = extract_data(csv_url)
+    data = transform_data(raw_data)
+    ingest_data(user, password, host, port, db, table_name, data)
 
 
 if __name__ == '__main__':
