944229996acacad52dd14c8697ea7cde46adab1a
joshuaati <joshuaati97@gmail.com>
2023-02-11 21:15:44 +0100

    bigquery sql addition

--- week_3_data_warehouse/bigQuery.sql
+++ week_3_data_warehouse/bigQuery.sql
@@ -8,9 +8,28 @@ OPTIONS(
 
 
 CREATE OR REPLACE TABLE 
-  dtc-de-375918.trips_data_all.external_2019_non_partitoned AS 
+  dtc-de-375918.trips_data_all.bqtable_2019_non_partitoned AS 
     SELECT * 
     FROM 
         dtc-de-375918.trips_data_all.external_2019_tripdata
 
-SELECT COUNT(DISTINCT(Affiliated_base_number)) FROM `dtc-de-375918.trips_data_all.external_2019_non_partitoned`
+
+SELECT COUNT(DISTINCT(Affiliated_base_number)) 
+FROM `dtc-de-375918.trips_data_all.bqtable_2019_non_partitoned`
+
+
+CREATE OR REPLACE TABLE 
+  dtc-de-375918.trips_data_all.bqtable_2019_partitoned_clustered
+  PARTITION BY DATE(pickup_datetime)
+  CLUSTER BY affiliated_base_number AS 
+    SELECT * 
+    FROM 
+        dtc-de-375918.trips_data_all.external_2019_tripdata;
+
+SELECT COUNT(*) 
+FROM `dtc-de-375918.trips_data_all.bqtable_2019_partitoned_clustered` 
+WHERE PUlocationID IS NULL AND DOlocationID IS NULL
+
+SELECT DISTINCT(Affiliated_base_number) 
+FROM `dtc-de-375918.trips_data_all.bqtable_2019_partitoned_clustered` 
+WHERE pickup_datetime BETWEEN '2019-03-01' AND '2019-03-31'
